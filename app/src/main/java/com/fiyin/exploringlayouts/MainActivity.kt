ce4c107b2f5e1add7ddcdb94b345390095beb373
fiyinTheSlim <fiyinoyekunle@gmail.com>
2021-07-17 18:15:49 +0100

    more

--- app/src/main/java/com/fiyin/exploringlayouts/MainActivity.kt
+++ app/src/main/java/com/fiyin/exploringlayouts/MainActivity.kt
@@ -2,12 +2,41 @@ package com.fiyin.exploringlayouts
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import android.view.View
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        Log.i("i", "The app  is created")
+        Toast.makeText(this, "The app is created at this point", Toast.LENGTH_SHORT).show()
         setContentView(R.layout.main_menu)
+    }
+    override fun onStart(){
+        super.onStart()
+        Log.i("i", "onStart is happening here")
+        Toast.makeText(this, "The app is being created at this point", Toast.LENGTH_SHORT).show()
+    }
+    override fun onResume(){
+        super.onResume()
+        Log.i("i", "The app is resuming")
+        Toast.makeText(this, "The app is reuming here", Toast.LENGTH_SHORT).show()
+    }
+    override fun onPause(){
+        super.onPause()
+        Log.i("i", "the app is being paused")
+        Toast.makeText(this, "The app is being paused here", Toast.LENGTH_SHORT).show()
+    }
+    override fun onStop(){
+        super.onStop()
+        Log.i("i", "The app is stopping")
+        Toast.makeText(this, "The app is stopping, so make changes", Toast.LENGTH_SHORT).show()
+    }
+    override fun onDestroy(){
+        super.onDestroy()
+        Log.i("i", "The app is detroyed so you all can be on your merry way")
+        Toast.makeText(this, "No going back, the app is destroyed", Toast.LENGTH_SHORT).show()
     }
     fun loadConstraintLayout(v: View){
         setContentView(R.layout.activity_main)
